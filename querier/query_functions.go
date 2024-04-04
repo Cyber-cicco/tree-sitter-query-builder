57b15e055a3be13bf6dab8005e3681c9e94ada0d
Abel Ciccoli <abelcoli@outlook.fr>
2024-04-04 11:50:23 +0200

    feat(query): added a query function to find the first node matching the result of a function

--- querier/query_functions.go
+++ querier/query_functions.go
@@ -19,3 +19,24 @@ func GetChildrenMatching(curr *sitter.Node, matcher matchingFunc, matched []*sit
 	}
     return matched
 }
+
+// Allows for searches in a node instead of a tree.
+// Recursively parses every subnodes of a given sitter node to find the first child matching
+// a pattern defined in a func.
+// If the node itself matches the function, returns itself as the first node matching the condition.
+func GetFirstMatch(curr *sitter.Node, matcher matchingFunc) *sitter.Node {
+
+    if matcher(curr) {
+        return curr
+    }
+
+	for i := 0; i < int(curr.ChildCount()); i++ {
+        matched := GetFirstMatch(curr.Child(i), matcher)
+
+        if matched != nil {
+            return matched
+        }
+	}
+
+    return nil
+}
--- querier/query_test.go
+++ querier/query_test.go
@@ -29,94 +29,153 @@ public class Routes {
     public static final String ADR_FORM_ERROR = "components/form-error";
 }
 `
+
 func init() {
-    parser = sitter.NewParser()
-    lang = java.GetLanguage()
-    parser.SetLanguage(lang)
+	parser = sitter.NewParser()
+	lang = java.GetLanguage()
+	parser.SetLanguage(lang)
 }
 
 func TestQuery(t *testing.T) {
 
-    parser := sitter.NewParser()
-    parser.SetLanguage(java.GetLanguage())
+	parser := sitter.NewParser()
+	parser.SetLanguage(java.GetLanguage())
 
-    qb := NewQB()
-    qb.
-    NewSExpression("field_declaration").
-        NewSExpression("modifiers").End().
-        NewSExpression("variable_declarator").
-            Prop("declarator").
-            NewSExpression("identifier").
-                Prop("name").
-                End().
-            NewSExpression("string_literal").
-                Prop("value").
-                NewSExpression("string_fragment").
-                Var("fragment", qb)
+	qb := NewQB()
+	qb.
+		NewSExpression("field_declaration").
+		NewSExpression("modifiers").End().
+		NewSExpression("variable_declarator").
+		Prop("declarator").
+		NewSExpression("identifier").
+		Prop("name").
+		End().
+		NewSExpression("string_literal").
+		Prop("value").
+		NewSExpression("string_fragment").
+		Var("fragment", qb)
 
-    qb.NewMatcher("match").Identifier("fragment").ValueAsString("page.+")
-    query := qb.Marshal()
-    query.Content = []byte(CONTENT)
-    query.Lang = lang
+	qb.NewMatcher("match").Identifier("fragment").ValueAsString("page.+")
+	query := qb.Marshal()
+	query.Content = []byte(CONTENT)
+	query.Lang = lang
 
-    tree, err := parser.ParseCtx(context.Background(), nil, query.Content)
+	tree, err := parser.ParseCtx(context.Background(), nil, query.Content)
 
-    if err != nil {
-        t.Fatalf("Shouldn't have had error, got %s", err)
-    }
+	if err != nil {
+		t.Fatalf("Shouldn't have had error, got %s", err)
+	}
 
-    query.Tree = tree
+	query.Tree = tree
 
-    captures := []*sitter.QueryCapture{}
-    err = query.ExecuteQuery(func(c *sitter.QueryCapture) error {captures = append(captures, c); return nil}) 
+	captures := []*sitter.QueryCapture{}
+	err = query.ExecuteQuery(func(c *sitter.QueryCapture) error { captures = append(captures, c); return nil })
 
-    if err != nil {
-        t.Fatalf("Shouldn't have had error, got %s", err)
-    }
+	if err != nil {
+		t.Fatalf("Shouldn't have had error, got %s", err)
+	}
 
-    if len(captures) != 3 {
-        t.Fatalf("Wrong number of captures. Expected 3, got %d", len(captures))
-    }
+	if len(captures) != 3 {
+		t.Fatalf("Wrong number of captures. Expected 3, got %d", len(captures))
+	}
 }
 
 func TestExecuteSimpleQuery(t *testing.T) {
 
-    qb := NewQB()
-    qb.
-    NewSExpression("field_declaration").
-        NewSExpression("modifiers").End().
-        NewSExpression("variable_declarator").
-            Prop("declarator").
-            NewSExpression("identifier").
-                Prop("name").
-                End().
-            NewSExpression("string_literal").
-                Prop("value").
-                NewSExpression("string_fragment").
-                Var("fragment", qb)
+	qb := NewQB()
+	qb.
+		NewSExpression("field_declaration").
+		NewSExpression("modifiers").End().
+		NewSExpression("variable_declarator").
+		Prop("declarator").
+		NewSExpression("identifier").
+		Prop("name").
+		End().
+		NewSExpression("string_literal").
+		Prop("value").
+		NewSExpression("string_fragment").
+		Var("fragment", qb)
 
-    qb.NewMatcher("match").Identifier("fragment").ValueAsString("page.+")
+	qb.NewMatcher("match").Identifier("fragment").ValueAsString("page.+")
 
-    query := qb.Marshal()
-    query.Content = []byte(CONTENT)
-    query.Lang = lang
+	query := qb.Marshal()
+	query.Content = []byte(CONTENT)
+	query.Lang = lang
 
-    tree, err := parser.ParseCtx(context.Background(), nil, query.Content)
+	tree, err := parser.ParseCtx(context.Background(), nil, query.Content)
 
-    if err != nil {
-        t.Fatalf("Shouldn't have had error, got %s", err)
-    }
+	if err != nil {
+		t.Fatalf("Shouldn't have had error, got %s", err)
+	}
 
-    query.Tree = tree
+	query.Tree = tree
 
-    captures := []*sitter.QueryCapture{}
-    err = query.ExecuteSimpleQuery(func(c *sitter.QueryCapture) error {captures = append(captures, c); return nil}) 
+	captures := []*sitter.QueryCapture{}
+	err = query.ExecuteSimpleQuery(func(c *sitter.QueryCapture) error { captures = append(captures, c); return nil })
 
-    if err != nil {
-        t.Fatalf("Shouldn't have had error, got %s", err)
-    }
+	if err != nil {
+		t.Fatalf("Shouldn't have had error, got %s", err)
+	}
 
-    if len(captures) != 5 {
-        t.Fatalf("Wrong number of captures. Expected 3, got %d", len(captures))
-    }
+	if len(captures) != 5 {
+		t.Fatalf("Wrong number of captures. Expected 5, got %d", len(captures))
+	}
+}
+
+func TestFirstMatch(t *testing.T) {
+
+	qb := NewQB()
+	qb.
+		NewSExpression("field_declaration").
+		Var("fragment", qb).
+		NewSExpression("modifiers").End().
+		NewSExpression("variable_declarator").
+		Prop("declarator").
+		NewSExpression("identifier").
+		Prop("name").
+		End().
+		NewSExpression("string_literal").
+		Prop("value").
+		NewSExpression("string_fragment")
+
+
+	query := qb.Marshal()
+	query.Content = []byte(CONTENT)
+	query.Lang = lang
+
+	tree, err := parser.ParseCtx(context.Background(), nil, query.Content)
+
+	if err != nil {
+		t.Fatalf("Shouldn't have had error, got %s", err)
+	}
+
+	query.Tree = tree
+
+	captures := []*sitter.QueryCapture{}
+	err = query.ExecuteSimpleQuery(func(c *sitter.QueryCapture) error { captures = append(captures, c); return nil })
+
+	if err != nil {
+		t.Fatalf("Shouldn't have had error, got %s", err)
+	}
+
+	if len(captures) != 5 {
+		t.Fatalf("Wrong number of captures. Expected 5, got %d", len(captures))
+	}
+
+	firstMatching := GetFirstMatch(captures[0].Node, func(node *sitter.Node) bool {
+		return node.Type() == "string_literal"
+	})
+
+	if firstMatching == nil {
+		t.Fatalf("expected a result, got nil")
+	}
+
+	noMatch := GetFirstMatch(captures[0].Node, func(node *sitter.Node) bool {
+			return node.Type() == "class_declaration"
+		})
+
+	if noMatch != nil {
+		t.Fatalf("expected nil, got %v", noMatch)
+	}
+
 }
